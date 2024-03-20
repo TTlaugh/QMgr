0fe0276afb841108a889233e4255fe1bc79298d3
Nguyen Le Tien Trien <nltt.xyz@gmail.com>
2024-03-20 15:58:25 +0700

    remove anchorpane

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,16 +3,17 @@ package ui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 
-			Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+			BorderPane root = (BorderPane) loader.load();
 
 			Scene scene = new Scene(root);
 //			scene.getStylesheets().add(getClass().getResource("/ui/application.css").toExternalForm());
