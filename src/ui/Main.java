a72ba706b8cf50bac3f7ae38095dae45772f9a45
Nguyen Le Tien Trien <nltt.xyz@gmail.com>
2024-03-20 14:38:42 +0700

    revert

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,27 +1,22 @@
 package ui;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.transform.Scale;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
-			BorderPane root = (BorderPane) loader.load();
+			Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
 
 			Scene scene = new Scene(root);
+//			scene.getStylesheets().add(getClass().getResource("/ui/application.css").toExternalForm());
+
 			primaryStage.setScene(scene);
 
 			primaryStage.getIcons().add(new Image("/images/icon.png"));
@@ -29,12 +24,6 @@ public class Main extends Application {
 
 			primaryStage.show();
 			primaryStage.setMaximized(true);
-
-			Screen screen = Screen.getPrimary();
-			double dpi = screen.getDpi();
-			double scaleFactor = dpi / 96.0;
-			primaryStage.setRenderScaleX(scaleFactor);
-			primaryStage.setRenderScaleY(scaleFactor);
 
 		} catch (Exception e) {
 			e.printStackTrace();
