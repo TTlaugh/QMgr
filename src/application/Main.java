634cdb1c1e816c000768911e804adaabf2835255
Nguyen Le Tien Trien <nltt.xyz@gmail.com>
2024-04-10 15:52:38 +0700

    runable Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			primaryStage.getIcons().add(new Image("/images/icon.png"));
+//			primaryStage.getIcons().add(new Image("/images/icon.png"));
 			primaryStage.setTitle("Mine Quizz - HEHE");
 //			primaryStage.initStyle(StageStyle.TRANSPARENT);
 //			primaryStage.initStyle(StageStyle.UNDECORATED);
