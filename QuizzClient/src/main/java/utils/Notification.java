1d435ec334696b5a3d09104348af658e48af73c2
Your Name <you@example.com>
2024-10-12 19:53:23 +0700

    clean up

--- QuizzClient/src/main/java/utils/Notification.java
+++ QuizzClient/src/main/java/utils/Notification.java
@@ -5,11 +5,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
 
-public class DisplayDialog_Notification {
+public class Notification {
 
-	public static void Dialog_Error(String title, String content, String header) {
-		Alert typeAlert=new Alert(AlertType.ERROR);
-		Button okButton = new Button("Cancel");			
+	public static void Error(String title, String content, String header) {
+		Alert typeAlert = new Alert(AlertType.ERROR);
+		Button okButton = new Button("Cancel");
 		okButton.setOnAction(event -> {
 			typeAlert.hide();
 		});
@@ -18,17 +18,19 @@ public class DisplayDialog_Notification {
 		typeAlert.setTitle(title);
 		typeAlert.showAndWait();
 	}
-	public static Alert Dialog_Comfrim( String title, String content, String header) {
-		Alert alert = new Alert(AlertType.CONFIRMATION, content, ButtonType.YES,ButtonType.CANCEL);
+
+	public static Alert Comfrim(String title, String content, String header) {
+		Alert alert = new Alert(AlertType.CONFIRMATION, content, ButtonType.YES, ButtonType.CANCEL);
 		alert.setContentText(content);
 		alert.setHeaderText(header);
 		alert.setTitle(title);
 		alert.showAndWait();
 		return alert;
 	}
-	public static void Dialog_Infomation(String title, String content, String header) {
-		Alert typeAlert=new Alert(AlertType.INFORMATION);
-		Button okButton = new Button("Cancel");			
+
+	public static void Infomation(String title, String content, String header) {
+		Alert typeAlert = new Alert(AlertType.INFORMATION);
+		Button okButton = new Button("Cancel");
 		okButton.setOnAction(event -> {
 			typeAlert.hide();
 		});
