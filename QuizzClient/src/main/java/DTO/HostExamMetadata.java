278658b48dad8ccdf0e5c157acbd838d8d3c76eb
Low Tee <lowtee.vn@gmail.com>
2024-10-24 02:28:25 +0700

    Done DTO Client + Server

---
+++ QuizzClient/src/main/java/DTO/HostExamMetadata.java
@@ -0,0 +1,13 @@
+package DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HostExamMetadata {
+    private String examName;
+    private String subject;
+}
